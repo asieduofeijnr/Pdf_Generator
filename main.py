013d734d8ac608948013cfdf678cd853a763ad6d
asieduofeijnr <114332208+asieduofeijnr@users.noreply.github.com>
2023-03-14 13:22:39 +0000

    day 24: add lines

--- main.py
+++ main.py
@@ -15,6 +15,12 @@ for index, row in df.iterrows():
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
     pdf.line(10, 22, 200, 22)
 
+
+    #duplication of line with interval at 10mm
+    for space in range(0, 260 , 10):
+        pdf.line(10, 32 + space, 200, 32 + space)
+
+
     #Set the footer
     pdf.ln(265)
     pdf.set_font(family="Times", style='I', size=8)
@@ -28,6 +34,10 @@ for index, row in df.iterrows():
         pdf.ln(277)
         pdf.set_font(family="Times", style='I', size=8)
         pdf.set_text_color(100, 180, 100)
-        pdf.cell(w=0, h=10, txt=row["Topic"], align="R")        
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+        #duplication of line with interval at 10mm
+        for space in range(0, 260 , 10):
+            pdf.line(10, 32 + space, 200, 32 + space)        
 
 pdf.output("output.pdf") 
